7bd9b0515982418e6400cea3032743d073619f90
samvelarakelyan <62077195+samvelarakelyan@users.noreply.github.com>
2021-07-26 14:27:12 +0400

    Update cars_parser.py

--- Web Parsing/Cars Parsing/cars_parser.py
+++ Web Parsing/Cars Parsing/cars_parser.py
@@ -1,6 +1,15 @@
 """
 Program to parsing cars data from 'https://auto.ria.com/' '/newauto' section
-*******************
+
+***************************
+*This program has been run*
+***************************
+	Processor -> 'AMD A6'
+	Platform  -> 'LINUX, Kernel verssion 5.11.0-16-generic'
+	OS        -> 'Unbuntu 21.04'
+This program is written only in python.
+Don't use non-standard libraries.
+
 WARNING!
 --------
     This script may not work if you рун it.
